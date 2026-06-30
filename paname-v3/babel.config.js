424ef7292a0f599303bce9f3858bdb38f349f4cd
MaxPyroli <max2501yt@gmail.com>
2026-06-30 19:00:39 +0200

    babel merde

--- paname-v3/babel.config.js
+++ paname-v3/babel.config.js
@@ -2,9 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // ⚠️ react-native-reanimated/plugin DOIT être le DERNIER plugin de la liste
-      'react-native-reanimated/plugin',
-    ],
   };
 };
